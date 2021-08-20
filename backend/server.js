0df9cf53af0fe8102db4cd56d98fdcc7c7e6cd24
jonjau <jonjau3q@gmail.com>
2021-08-20 15:18:16 +1000

    backend fix

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 // Set the port of our server
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 // Add some middleware
 app.use((req, res, next) => {
