12f8c062e645932409a5cc257168c77a6c6c1817
jonjau <jonjau3q@gmail.com>
2021-08-20 14:48:59 +1000

    copied code from React workshop

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import { Container } from "@material-ui/core";
+import Router from "./Router";
+
+function App() {
+  return (
+    <Container>
+      <Router />
+    </Container>
+  );
+}
+
+export default App;
